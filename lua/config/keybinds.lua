2e7f0b43bc137135ccb8dd39ab66b67924a1c049
Jean <jean.dahl.dev@gmail.com>
2025-08-31 14:17:48 +0200

    feat: keymap

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -26,3 +26,6 @@ vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
 vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", opts) -- close buffer
 vim.keymap.set("n", "<leader>b", "<cmd> enew <CR>", opts) -- new buffer
+
+-- terminal
+map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
